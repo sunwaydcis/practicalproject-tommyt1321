a70b700a82630de3c07500bc9005327f553a7309
tommyt1321 <yongwei1321@gmail.com>
2025-06-24 18:11:05 +0800

    Week8
    Week8

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,16 +1,36 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+  /**
+   * The data as an observable list of Persons.
+   */
+  val personData = new ObservableBuffer[Person]()
+
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster")
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
 
   override def start(): Unit =
     // transform path of RootLayout.fxml to URI for resource location.
@@ -37,3 +57,22 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
+
+  val aString = new StringProperty("sunway") //publisher
+  val bString = new StringProperty("monash") //subscriber
+
+  bString <==> aString
+  aString.onChange((a, b, c) => {
+    println("aString changed from " + b + " to " + c)
+  })
+  aString.onChange((_, _, c) => {
+    println(c + " is updated in aString")
+  })
+  aString.value = "segi"
+  bString.value = "monash"
+  println(aString.value)
+  println(bString.value)
+
+  val add: (Int, Int) => Int = (_, _) =>
+    12
+  println(add(1,2))
